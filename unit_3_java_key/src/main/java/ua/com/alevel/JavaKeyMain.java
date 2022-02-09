335f43723c16387d4c2c28cad486613dfdefdca5
Iegor-Funtusov <funtushan@gmail.com>
2022-02-09 16:11:29 +0200

    full circle jar

--- unit_3_java_key/src/main/java/ua/com/alevel/JavaKeyMain.java
+++ unit_3_java_key/src/main/java/ua/com/alevel/JavaKeyMain.java
@@ -1,8 +1,19 @@
 package ua.com.alevel;
 
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 public class JavaKeyMain {
 
     public static void main(String[] args) {
         ProgramRun.start();
+        double[] values = new double[] {65, 51 , 16, 11 , 6519, 191 ,0 , 98, 19854, 1, 32};
+        DescriptiveStatistics descriptiveStatistics = new DescriptiveStatistics();
+        for (double v : values) {
+            descriptiveStatistics.addValue(v);
+        }
+
+        double mean = descriptiveStatistics.getMean();
+        double median = descriptiveStatistics.getPercentile(50);
+        double standardDeviation = descriptiveStatistics.getStandardDeviation();
     }
 }
